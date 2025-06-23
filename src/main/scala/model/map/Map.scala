428c3a5ce18475f673d93e7fb438a566806dccf0
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-06-23 20:27:32 +0200

    feat: impl Route

--- src/main/scala/model/map/Map.scala
+++ src/main/scala/model/map/Map.scala
@@ -7,3 +7,24 @@ object City:
   def apply(name: String): City = CityImpl(name)
 
   private case class CityImpl(override val name: String) extends City
+
+trait Route:
+  import Route.Mechanic
+  def connectedCities: (City, City)
+  def length: Int
+  def mechanic: Mechanic
+
+object Route:
+  trait Mechanic
+
+  private type Color = String // TODO to be integrated with utils.Color
+  trait SpecificColor(color: Color) extends Mechanic
+  object SpecificColor:
+    def apply(color: String): SpecificColor = SpecificColorImpl(color)
+    private case class SpecificColorImpl(color: Color) extends SpecificColor(color)
+
+  def apply(connectedCities: (City, City), length: Int, mechanic: Mechanic): Route =
+    RouteImpl(connectedCities, length, mechanic)
+
+  private case class RouteImpl(override val connectedCities: (City, City), override val length: Int,
+                               override val mechanic: Mechanic) extends Route
--- src/test/scala/model/map/MapTest.scala
+++ src/test/scala/model/map/MapTest.scala
@@ -8,3 +8,12 @@ class MapTest extends AnyFlatSpec with Matchers:
     val name = "Roma"
     val city = City(name)
     city.name should be(name)
+
+  "A Route" should "be initialized correctly" in:
+    val connectedCities = (City("Roma"), City("Venezia"))
+    val length = 2
+    val mechanic = Route.SpecificColor("Black") // TODO to be integrated with utils.Color
+    val route = Route(connectedCities, length, mechanic)
+    route.connectedCities should be(connectedCities)
+    route.length should be(length)
+    route.mechanic should be(mechanic)

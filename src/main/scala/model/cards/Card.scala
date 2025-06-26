ed32150d64a6e9440b401f9c3394c2c89e08909f
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-06-26 17:12:56 +0200

    add scaladoc

--- src/main/scala/model/cards/Card.scala
+++ src/main/scala/model/cards/Card.scala
@@ -2,13 +2,27 @@ package model.cards
 
 import model.utils.Color
 
+/** A train card characterized by a color. */
 trait Card:
+  /** The color of the card.
+    *
+    * @return
+    *   the color of the card.
+    */
   def color: Color
 
+/** Factory for [[Card]] instances. */
 object Card:
+  /** Create a card of the given color.
+    *
+    * @param color
+    *   the color of the card created.
+    * @return
+    *   the card created.
+    */
   def apply(color: Color): Card = CardImpl(color)
   private case class CardImpl(override val color: Color) extends Card
 
-private abstract class Cards(private var _cards: List[Card]):
+private class Cards(private var _cards: List[Card]):
   def cards: List[Card] = _cards
   def cards_=(cards: List[Card]): Unit = _cards = cards
--- src/main/scala/model/cards/Deck.scala
+++ src/main/scala/model/cards/Deck.scala
@@ -1,20 +1,43 @@
 package model.cards
 
+/** A deck of train cards, which can be shuffled and cards can be drawn from it. */
 trait Deck extends Cards:
   import scala.util.Random
 
+  /** Shuffle the deck to reorder it randomly. */
   def shuffle(): Unit =
     cards = Random.shuffle(cards)
+
+  /** Draw the given amount of cards from the top of the deck.
+    *
+    * @param n
+    *   the number of cards to draw.
+    * @return
+    *   the list of cards drawn.
+    */
   def draw(n: Int): List[Card] =
     val result = cards.splitAt(n)
     cards = result._2
     result._1
+
+  /** Reinsert the given card at the bottom of the deck.
+    *
+    * @param card
+    *   the card to reinsert at the bottom of the deck.
+    */
   def reinsertAtTheBottom(card: Card): Unit =
     cards = cards :+ card
 
+/** A generator of a deck of train cards, that generates the list of train cards composing the deck. */
 trait DeckGenerator:
+  /** Generate the list of train cards composing the deck.
+    *
+    * @return
+    *   the list of train cards composing the deck.
+    */
   def generate(): List[Card]
 
+/** The standard deck generator according to the rules. */
 given defaultDeckGenerator: DeckGenerator with
   override def generate(): List[Card] =
     import model.utils.Color
@@ -27,6 +50,11 @@ given defaultDeckGenerator: DeckGenerator with
     yield list = list :+ Card(color)
     list
 
+/** A deck of train cards.
+  *
+  * @param generator
+  *   the deck generator.
+  */
 class CardsDeck()(using generator: DeckGenerator)
     extends Cards(generator.generate())
     with Deck

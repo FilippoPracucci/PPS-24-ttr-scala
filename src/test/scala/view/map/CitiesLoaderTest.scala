783824940e6091e7b996318c4ff6216db126b648
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-02 19:35:00 +0200

    refactor and remove useless test (running headless...by default allowed on Windows but not in Linux. It seems quite fair to remove it)

--- src/test/scala/view/map/CitiesLoaderTest.scala
+++ src/test/scala/view/map/CitiesLoaderTest.scala
@@ -9,15 +9,11 @@ class CitiesLoaderTest extends AnyFlatSpec with Matchers:
   private val illegalParam = -10
   import CitiesLoader.given
 
-  "A CitiesLoader" should "load the cities" in:
-    val citiesLoader = CitiesLoader(mapWidth, mapHeight)
-    noException should be thrownBy citiesLoader.load()
+  "A CitiesLoader" should "fail to initialize if the given map width or height aren't positive numbers" in:
+    an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, mapHeight)
+    an[IllegalArgumentException] should be thrownBy CitiesLoader(mapWidth, illegalParam)
+    an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, illegalParam)
 
   it should "fail to load the cities if the given config file doesn't exist" in:
     val citiesLoader = CitiesLoader(mapWidth, mapHeight)(using "nonExistentFile.json")
     an[IllegalStateException] should be thrownBy citiesLoader.load()
-
-  it should "fail to initialize if the given map width or height aren't positive numbers" in:
-    an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, mapHeight)
-    an[IllegalArgumentException] should be thrownBy CitiesLoader(mapWidth, illegalParam)
-    an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, illegalParam)

c63816b443a2e2e3a0d4f95015944a1e927cfb9b
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-02 18:48:44 +0200

    refactor: change test name

--- src/test/scala/view/map/CitiesLoaderTest.scala
+++ src/test/scala/view/map/CitiesLoaderTest.scala
@@ -17,7 +17,7 @@ class CitiesLoaderTest extends AnyFlatSpec with Matchers:
     val citiesLoader = CitiesLoader(mapWidth, mapHeight)(using "nonExistentFile.json")
     an[IllegalStateException] should be thrownBy citiesLoader.load()
 
-  it should "fail to load the cities if the given map width or height aren't positive numbers" in:
+  it should "fail to initialize if the given map width or height aren't positive numbers" in:
     an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, mapHeight)
     an[IllegalArgumentException] should be thrownBy CitiesLoader(mapWidth, illegalParam)
     an[IllegalArgumentException] should be thrownBy CitiesLoader(illegalParam, illegalParam)

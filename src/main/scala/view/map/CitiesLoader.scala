ebb9825ee93597b2b4821a2bd32ee9a393af86e4
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-04 12:37:09 +0200

    improve CitiesLoader and add missing city in cities.json

--- src/main/scala/view/map/CitiesLoader.scala
+++ src/main/scala/view/map/CitiesLoader.scala
@@ -8,38 +8,41 @@ trait CitiesLoader:
   def load(): Unit
 
 object CitiesLoader:
-  /** The default name of the config file (contained in 'src/main/resources/').
+  /** The default path of the json config file (starting from 'src/main/resources/').
     */
-  given defaultConfigFileName: String = "cities.json"
+  given defaultConfigFilePath: String = "cities"
 
   /** Creates a `CitiesLoader`.
     * @param mapWidth
     *   the width (in pixel) of the map
     * @param mapHeight
     *   the height (in pixel) of the map
-    * @param configFileName
-    *   the given name of the config file (in 'src/main/resources/') containing information on the cities
+    * @param configFilePath
+    *   the given path of the json config file (starting from 'src/main/resources/', without file extension) containing
+   *    information on the cities
     * @return
     *   the created `CitiesLoader`
     */
-  def apply(mapWidth: Int, mapHeight: Int)(using configFileName: String): CitiesLoader =
-    CitiesLoaderImpl(mapWidth, mapHeight)(using configFileName)
+  def apply(mapWidth: Int, mapHeight: Int)(using configFilePath: String): CitiesLoader =
+    CitiesLoaderImpl(mapWidth, mapHeight)(using configFilePath)
 
-  private class CitiesLoaderImpl(mapWidth: Int, mapHeight: Int)(using configFileName: String) extends CitiesLoader:
+  private class CitiesLoaderImpl(mapWidth: Int, mapHeight: Int)(using configFilePath: String) extends CitiesLoader:
     require(mapWidth > 0, "mapWidth must be positive")
     require(mapHeight > 0, "mapHeight must be positive")
 
     import scala.util._
 
+    private val fileExtension = ".json"
+    private val errorMessage = "Error loading config file: "
     private val cityWidth = 0
     private val cityHeight = 0
 
     private case class City(name: String, x: Int, y: Int)
-    private case class ConfigData(scaleWidth: Double, scaleHeight: Double, cities: Seq[City])
+    private case class ConfigData(scaleWidth: Double, scaleHeight: Double, cities: Set[City])
 
     override def load(): Unit = readConfigData() match
       case Success(configData) => configData.cities.foreach(city => addCity(configData, city))
-      case Failure(e) => throw new IllegalStateException(s"Error loading config file: ${e.getMessage}", e)
+      case Failure(e) => throw new IllegalStateException(errorMessage + e.getMessage, e)
 
     private def readConfigData(): Try[ConfigData] =
       import scala.io.Source
@@ -48,7 +51,7 @@ object CitiesLoader:
       given ReadWriter[City] = macroRW
       given ReadWriter[ConfigData] = macroRW
 
-      Using(Source.fromResource(configFileName)) { source => read[ConfigData](source.mkString) }
+      Using(Source.fromResource(configFilePath + fileExtension)) { source => read[ConfigData](source.mkString) }
 
     private def addCity(configData: ConfigData, city: City): Unit =
       MapView().addCity(city.name, city.x / configData.scaleWidth * mapWidth,

5806f2df92d09a8b292f228f834b0d82d5c53b93
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-04 16:07:07 +0200

    feat: load routes from model into view (length only)

--- src/main/scala/controller/GameController.scala
+++ src/main/scala/controller/GameController.scala
@@ -13,14 +13,20 @@ object GameController:
   def apply(): GameController = GameControllerImpl
 
   private object GameControllerImpl extends GameController:
+    import model.map.GameMap
+    import GameMap.given
     import view.GameView
+
+    private val gameMap = GameMap()
     private val gameView = GameView()
 
     initGameView()
 
     private def initGameView(): Unit =
-      gameView.addRoute(("Roma", "Palermo"), 4)
-      gameView.addRoute(("Roma", "Venezia"), 2)
-      gameView.addRoute(("Roma", "Brindisi"), 2)
-      gameView.addRoute(("Palermo", "Brindisi"), 3)
+      gameMap.routes.foreach(route =>
+        gameView.addRoute(
+          (route.connectedCities._1.name, route.connectedCities._2.name),
+          route.length
+        )
+      )
       gameView.open()

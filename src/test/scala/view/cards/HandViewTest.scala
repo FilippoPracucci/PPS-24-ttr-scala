35704a99bf1f4ce2759b34344cb8c230d92eb143
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-07-10 17:08:52 +0200

    refactor: automitized draw cards button click

--- src/test/scala/view/cards/HandViewTest.scala
+++ src/test/scala/view/cards/HandViewTest.scala
@@ -1,6 +1,7 @@
 package view.cards
 
 import scala.swing.*
+import scala.swing.event.ButtonClicked
 
 /** A simple test consisting in launching a frame with a player's hand of train cards to show its representation. */
 object HandViewTest extends App:
@@ -9,28 +10,41 @@ object HandViewTest extends App:
   import model.cards.{Card, CardsGenerator, Deck, Hand}
   import controller.GameController.*
 
-  val fixedList: List[Card] = List(Card(RED), Card(YELLOW), Card(RED), Card(BLACK), Card(BLUE))
-  val fixedDeckGenerator: CardsGenerator[Deck] = () => fixedList
-  val deckFixed: Deck = Deck()(using fixedDeckGenerator)
-  val hand: Hand = Hand(deckFixed)
+  private val fixedList: List[Card] = List(Card(RED), Card(YELLOW), Card(RED), Card(BLACK), Card(BLUE))
+  private val fixedDeckGenerator: CardsGenerator[Deck] = () => fixedList
+  private val deckFixed: Deck = Deck()(using fixedDeckGenerator)
+  private val hand: Hand = Hand(deckFixed)
 
-  val handView = HandView(hand.cards.map(c => CardView(c.colorName)(c.cardColor, c.cardTextColor)))
-  val frame = Frame()
+  private val handView = HandView(hand.cards.map(c => CardView(c.colorName)(c.cardColor, c.cardTextColor)))
+  private val frame = new MainFrame()
+  private val panel = new BoxPanel(Orientation.Horizontal)
+  private val button = new Button("Draw")
   import javax.swing.WindowConstants.EXIT_ON_CLOSE
 
   frame.peer.setDefaultCloseOperation(EXIT_ON_CLOSE)
   frame.visible = true
   frame.pack()
   frame.centerOnScreen()
-  frame.contents = handView.handComponent
+  panel.contents += handView.handComponent
+  panel.contents += button
+  frame.contents = panel
+
+  button.listenTo(button.mouse.clicks)
+  Reactions.Impl()
+  button.reactions += {
+    case _: ButtonClicked => updateHandView()
+    case _ => ()
+  }
 
   Thread.sleep(1000)
-  updateHandView()
+  button.doClick()
 
   private def updateHandView(): Unit =
     val cardsToAdd = List(Card(GREEN), Card(BLUE))
     hand.addCards(cardsToAdd)
+    panel.contents.clear()
     handView.addCardsComponent(cardsToAdd.map(c => CardView(c.colorName)(c.cardColor, c.cardTextColor)))
-    frame.contents = handView.handComponent
+    panel.contents += handView.handComponent
+    panel.contents += button
     frame.repaint()
     frame.pack()

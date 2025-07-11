7cb174f2b01f2e23e986ed55caaa798347e37c62
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-11 12:48:40 +0200

    refactor GameView: reordered methods and new private object for better clarity; improve initMap method

--- src/main/scala/view/GameView.scala
+++ src/main/scala/view/GameView.scala
@@ -61,18 +61,13 @@ object GameView:
   def apply(): GameView = GameViewSwing
 
   private object GameViewSwing extends GameView:
-    import scala.swing._
+    import scala.swing.*
+    import event.MousePressed
     import ScrollPane.BarPolicy.*
-    import scala.swing.event.MousePressed
     import controller.GameController
 
     private val screenSize: Dimension = java.awt.Toolkit.getDefaultToolkit.getScreenSize
-    private val mapView = MapView()
     private val panel = new BoxPanel(Orientation.Vertical)
-    private val southPanel = new BoxPanel(Orientation.Horizontal)
-    private val handPanel = new BoxPanel(Orientation.Horizontal)
-    private val scrollPane = new ScrollPane(handPanel)
-    private val handButtonPanel = new BoxPanel(Orientation.Vertical)
     private val frame: MainFrame = new MainFrame {
       title = "Ticket to Ride"
       contents = panel
@@ -80,11 +75,52 @@ object GameView:
       peer.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH)
       resizable = false
     }
-    private val gameController: GameController = GameController()
+
+    private val southPanel = new BoxPanel(Orientation.Horizontal)
+    private val handPanel = new BoxPanel(Orientation.Horizontal)
+    private val scrollPane = new ScrollPane(handPanel)
+    private val handButtonPanel = new BoxPanel(Orientation.Vertical)
     private val drawButton = new Button("Draw")
     private val groupByColorButton = new Button("Group by color")
-    configDrawButton()
-    configGroupByColorButton()
+
+    private val mapView = MapView()
+
+    private val gameController: GameController = GameController()
+
+    InitHelper.initPanels()
+
+    private object InitHelper:
+      def initPanels(): Unit =
+        handButtonPanel.contents += drawButton
+        configDrawButton()
+        handButtonPanel.contents += groupByColorButton
+        configGroupByColorButton()
+        scrollPane.horizontalScrollBarPolicy = AsNeeded
+        scrollPane.verticalScrollBarPolicy = Never
+        southPanel.contents += scrollPane
+        southPanel.contents += handButtonPanel
+        panel.contents += mapView.component
+        initMap()
+        panel.contents += southPanel
+        frame.repaint()
+
+      private def configDrawButton(): Unit =
+        drawButton.listenTo(drawButton.mouse.clicks)
+        drawButton.reactions += {
+          case _: MousePressed => gameController.drawCards(2)
+          case _ => ()
+        }
+
+      private def configGroupByColorButton(): Unit =
+        groupByColorButton.listenTo(groupByColorButton.mouse.clicks)
+        groupByColorButton.reactions += {
+          case _: MousePressed => gameController.groupCardsByColor()
+          case _ => ()
+        }
+
+      private def initMap(): Unit =
+        import CitiesLoader.given
+        CitiesLoader(screenSize.width, screenSize.height - southPanel.peer.getPreferredSize.getHeight.toInt).load()
 
     override def addHandsView(handsView: List[HandView]): Unit =
       handPanel.contents ++= handsView.map(_.handComponent)
@@ -93,39 +129,6 @@ object GameView:
       handPanel.contents.clear()
       addHandsView(handsView)
       frame.validate()
-
-    handButtonPanel.contents += drawButton
-    handButtonPanel.contents += groupByColorButton
-
-    initMap()
-
-    scrollPane.horizontalScrollBarPolicy = AsNeeded
-    scrollPane.verticalScrollBarPolicy = Never
-    southPanel.contents += scrollPane
-    southPanel.contents += handButtonPanel
-    panel.contents += mapView.component
-    panel.contents += southPanel
-    frame.repaint()
-
-    private def initMap(): Unit =
-      val topBarHeight = frame.peer.getInsets.top
-      val textHeight = 10
-      import CitiesLoader.given
-      CitiesLoader(screenSize.width, screenSize.height - topBarHeight - textHeight).load()
-
-    private def configDrawButton(): Unit =
-      drawButton.listenTo(drawButton.mouse.clicks)
-      drawButton.reactions += {
-        case _: MousePressed => gameController.drawCards(2)
-        case _ => ()
-      }
-
-    private def configGroupByColorButton(): Unit =
-      groupByColorButton.listenTo(groupByColorButton.mouse.clicks)
-      groupByColorButton.reactions += {
-        case _: MousePressed => gameController.groupCardsByColor()
-        case _ => ()
-      }
 
     override def reportError(message: String): Unit = Dialog.showMessage(frame, message, title = "Error")
 

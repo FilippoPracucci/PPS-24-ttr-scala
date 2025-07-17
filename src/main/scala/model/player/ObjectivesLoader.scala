057385355042f019347581ccba9902dd27dda11b
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-17 11:01:36 +0200

    fix: change Objective class name

--- src/main/scala/model/player/ObjectivesLoader.scala
+++ src/main/scala/model/player/ObjectivesLoader.scala
@@ -3,27 +3,29 @@ package model.player
 import utils.{LoaderFromFile, JsonReader}
 
 // TODO to integrate with the real Objective
-private case class Objective(citiesToConnect: (String, String), points: Int)
+private case class ObjectiveTemp(citiesToConnect: (String, String), points: Int)
 
 /** Class that represents a loader of objectives from a JSON file.
- * @param configFilePath 
- *   the given path of the JSON config file (starting from 'src/main/resources/', without file extension) containing
- *   the routes
- */
-class ObjectivesLoader()(using override val configFilePath: String) extends LoaderFromFile[Set[Objective]]
+  * @param configFilePath
+  *   the given path of the JSON config file (starting from 'src/main/resources/', without file extension) containing
+  *   the routes
+  */
+class ObjectivesLoader()(using override val configFilePath: String) extends LoaderFromFile[Set[ObjectiveTemp]]
     with JsonReader:
   import upickle.default.*
 
   /** Class that represents the structure of an objective in the JSON file.
-   * @param citiesToConnect the pair of cities to connect to complete the objective
-   * @param points the points the objective gives after it is completed
-   */
+    * @param citiesToConnect
+    *   the pair of cities to connect to complete the objective
+    * @param points
+    *   the points the objective gives after it is completed
+    */
   protected case class ObjectiveJson(citiesToConnect: (CityJson, CityJson), points: Int)
 
   /** Class that represents a city in the JSON file.
-   * @param name
-   *   the name of the city
-   */
+    * @param name
+    *   the name of the city
+    */
   protected case class CityJson(name: String)
 
   override protected type Data = Set[ObjectiveJson]
@@ -32,11 +34,13 @@ class ObjectivesLoader()(using override val configFilePath: String) extends Load
   protected given ReadWriter[CityJson] = macroRW
   override protected given readWriter: ReadWriter[Data] = readwriter[Seq[ObjectiveJson]].bimap[Data](_.toSeq, _.toSet)
 
-  override protected def onSuccess(objectivesJson: Data): Set[Objective] =
+  override protected def onSuccess(objectivesJson: Data): Set[ObjectiveTemp] =
     objectivesJson.map(objectiveJson =>
-      Objective((objectiveJson.citiesToConnect._1.name, objectiveJson.citiesToConnect._2.name), objectiveJson.points))
+      ObjectiveTemp((objectiveJson.citiesToConnect._1.name, objectiveJson.citiesToConnect._2.name),
+        objectiveJson.points)
+    )
 
 object ObjectivesLoader:
   /** The default path of the JSON config file (starting from 'src/main/resources/', without file extension).
-   */
+    */
   given defaultConfigFilePath: String = "objectives"

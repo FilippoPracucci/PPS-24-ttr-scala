18033c7287fbed31171a9959d645b80240131e85
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-07-17 16:02:59 +0200

    refactor: improve objective test, also by including the apply and unapply methods of Objective

--- src/test/scala/model/player/ObjectiveTest.scala
+++ src/test/scala/model/player/ObjectiveTest.scala
@@ -11,29 +11,35 @@ class BasicObjectiveTest extends AnyFlatSpec with Matchers:
   private val basicObjective = BasicObjective(cities, points)
 
   "A basic objective" should "be created correctly" in:
-    basicObjective.citiesToConnect should be(cities)
-    basicObjective.points should be(points)
+    noException should be thrownBy basicObjective
+    basicObjective should be(Objective(cities, points))
+    basicObjective.unapply() should be(Option(cities, points))
 
   it should "not be created with illegal values" in:
-    a[IllegalArgumentException] should be thrownBy BasicObjective(("Roma", "Roma"), 4)
-    a[IllegalArgumentException] should be thrownBy BasicObjective(("Roma", "Paris"), 0)
-    a[IllegalArgumentException] should be thrownBy BasicObjective(("Roma", "Roma"), -1)
+    a[IllegalArgumentException] should be thrownBy allOf(
+      BasicObjective(("Roma", "Roma"), 4),
+      BasicObjective(("Roma", "Paris"), 0),
+      BasicObjective(("Roma", "Roma"), -1)
+    )
 
 class ObjectiveWithCompletionTest extends AnyFlatSpec with Matchers:
 
   private val objectiveWithCompletion: ObjectiveCompletion = ObjectiveWithCompletion(cities, points)
 
   "An objective with completion" should "be created correctly" in:
+    noException should be thrownBy objectiveWithCompletion
     objectiveWithCompletion.citiesToConnect should be(cities)
     objectiveWithCompletion.points should be(points)
     objectiveWithCompletion.completed should be(false)
 
   it should "not be created with illegal values" in:
-    a[IllegalArgumentException] should be thrownBy ObjectiveWithCompletion(("Roma", "Roma"), 4)
-    a[IllegalArgumentException] should be thrownBy ObjectiveWithCompletion(("Roma", "Paris"), -2)
-    a[IllegalArgumentException] should be thrownBy ObjectiveWithCompletion(("Roma", "Roma"), 0)
+    a[IllegalArgumentException] should be thrownBy allOf(
+      ObjectiveWithCompletion(("Roma", "Roma"), 4),
+      ObjectiveWithCompletion(("Roma", "Paris"), -2),
+      ObjectiveWithCompletion(("Roma", "Roma"), 0)
+    )
 
-  it should "be markable as completed" in:
+  it should "be able to be marked as completed" in:
     objectiveWithCompletion.completed should be(false)
     objectiveWithCompletion.markAsComplete()
     objectiveWithCompletion.completed should be(true)

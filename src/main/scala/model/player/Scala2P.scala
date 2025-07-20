f5aca465fc0f21f70f6754e44d4610202e6f94e0
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-07-20 16:28:23 +0200

    add scaladoc to Scala2P

--- src/main/scala/model/player/Scala2P.scala
+++ src/main/scala/model/player/Scala2P.scala
@@ -1,18 +1,31 @@
 package model.player
 
+/** Object that provides utility methods to set theories and resolve Prolog goals in Scala. Adapted from
+  * https://github.com/unibo-pps/pps-lab12/blob/master/src/main/scala/it/unibo/u12lab/code/Scala2P.scala
+  */
 object Scala2P:
   import alice.tuprolog.*
+  import scala.language.implicitConversions
 
-  def extractTerm(solveInfo: SolveInfo, i: Integer): Term =
-    solveInfo.getSolution.asInstanceOf[Struct].getArg(i).getTerm
-
-  def extractTerm(solveInfo: SolveInfo, s: String): Term =
-    solveInfo.getTerm(s)
-
+  /** Implicit conversion from `String` to `Term`.
+    */
   given Conversion[String, Term] = Term.createTerm(_)
-  given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
-  given Conversion[String, Theory] = new Theory(_);
 
+  /** Implicit conversion from `Seq[_]` to `Term`.
+    */
+  given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
+
+  /** Implicit conversion from `String` to `Theory`.
+    */
+  given Conversion[String, Theory] = Theory.parseWithStandardOperators(_)
+
+  /** Creates a Prolog engine from the specified theory.
+    *
+    * @param theory
+    *   the Prolog theory
+    * @return
+    *   the created Prolog engine
+    */
   def mkPrologEngine(theory: Theory): Term => LazyList[SolveInfo] =
     val engine = Prolog()
     engine.setTheory(theory)
@@ -32,8 +45,14 @@ object Scala2P:
         }
       }.to(LazyList)
 
+  /** Solves the specified goal using the specified Prolog engine.
+    *
+    * @param engine
+    *   the Prolog engine to use
+    * @param goal
+    *   the goal to solve
+    * @return
+    *   true if the goal can be resolved successfully, false otherwise
+    */
   def solveWithSuccess(engine: Term => LazyList[SolveInfo], goal: Term): Boolean =
     engine(goal).map(_.isSuccess).headOption.contains(true)
-
-  def solveOneAndGetTerm(engine: Term => LazyList[SolveInfo], goal: Term, term: String): Term =
-    engine(goal).headOption.map(extractTerm(_, term)).get
